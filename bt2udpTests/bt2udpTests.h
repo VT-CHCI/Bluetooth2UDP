e12ddd8f23ace3d60c309ebbd078588ec7eb3706
Felipe Bacim <fbacim@vt.edu>
2013-09-15 11:33:25 -0400

    working copy

---
+++ bt2udpTests/bt2udpTests.h
@@ -0,0 +1,13 @@
+//
+//  bt2udpTests.h
+//  bt2udpTests
+//
+//  Created by Siroberto Scerbo on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface bt2udpTests : SenTestCase
+
+@end
---
+++ bt2udpTests/bt2udpTests.m
@@ -0,0 +1,32 @@
+//
+//  bt2udpTests.m
+//  bt2udpTests
+//
+//  Created by Siroberto Scerbo on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "bt2udpTests.h"
+
+@implementation bt2udpTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in bt2udpTests");
+}
+
+@end
